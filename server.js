b56af70fd3ca62e4b438544f4527cbe6f149e329
wemmbley <golevv.rustam@gmail.com>
2020-07-14 20:33:33 +0300

    local video display

---
+++ server.js
@@ -0,0 +1,8 @@
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer(
+    {
+        port: 9000,
+        path: '/myapp'
+    }
+);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 'use strict';
 
-let Peer = require('simple-peer')
-
 let sendError = (message) => {
     let error = document.querySelector('.error');
 
     error.innerHTML = message;
 };
 
-// get video/voice stream
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-})
-    .then(gotMedia)
-    .catch((error) => {
-        switch(error.name) {
+if(location.protocol !== 'https:') {
+    sendError('* Sorry, but HTTPS required');
+    throw new Error("Connection not secure.");
+}
+
+function getVideo(callbacks) {
+    navigator.getUserMedia = (
+        navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    const constraints = {
+        video: true,
+        audio: true
+    };
+
+    navigator.getUserMedia(constraints, callbacks.success, callbacks.error)
+}
+
+function displayStream(stream, element) {
+    let video = document.querySelector(element);
+
+    if(!video) {
+        sendError('* Sorry, but your browser does not support video');
+        throw new Error("Browser does not support video.");
+    }
+
+    video.srcObject = stream;
+}
+
+getVideo({
+    success: (stream) => {
+        displayStream(stream, '.local-video')
+    },
+    error: (error) => {
+        switch (error.name) {
             case 'NotFoundError': {
                 sendError('* Sorry, but you dont have any video devices');
                 break;
@@ -33,32 +60,57 @@ navigator.mediaDevices.getUserMedia({
                 console.log(error.name);
             }
         }
-    })
+    }
+})
 
-function gotMedia (stream) {
-    let peer1 = new Peer({ initiator: true, stream: stream })
-    let peer2 = new Peer()
 
-    peer1.on('signal', data => {
-        peer2.signal(data)
-    })
 
-    peer2.on('signal', data => {
-        peer1.signal(data)
-    })
+import Peer from 'peerjs';
+const localPeer = new Peer('user-1', {
+    host: '127.0.0.1:9000/myapp',
+    port: 443,
+    debug: 3
+})
 
-    peer2.on('stream', stream => {
-        // got remote video stream, now let's show it in a video tag
-        let video = document.querySelector('.user');
-        let remoteVideo = document.querySelector('.remote-user')
+localPeer.on('open', function(id) {
+    console.log('My peer ID is: ' + id);
+});
 
-        if ('srcObject' in video) {
-            remoteVideo.srcObject = stream
-        } else {
-            remoteVideo.src = window.URL.createObjectURL(stream) // for older browsers
-        }
-
-        video.src = window.stream;
-        remoteVideo.play()
-    })
-}
+// function gotMedia (stream) {
+//     let peer1 = new Peer({ initiator: true, stream: stream })
+//     let peer2 = new Peer({ stream: stream })
+//
+//     peer1.on('signal', data => {
+//         peer2.signal(data)
+//     })
+//
+//     peer2.on('signal', data => {
+//         peer1.signal(data)
+//     })
+//
+//     peer2.on('stream', stream => {
+//         // got remote video stream, now let's show it in a video tag
+//         let remoteVideo = document.querySelector('.remote-user')
+//
+//         if ('srcObject' in video) {
+//             remoteVideo.srcObject = stream
+//         } else {
+//             remoteVideo.src = window.URL.createObjectURL(stream) // for older browsers
+//         }
+//
+//         remoteVideo.play()
+//     })
+//
+//     peer1.on('stream', stream => {
+//         // got remote video stream, now let's show it in a video tag
+//         let remoteVideo = document.querySelector('.user')
+//
+//         if ('srcObject' in video) {
+//             remoteVideo.srcObject = stream
+//         } else {
+//             remoteVideo.src = window.URL.createObjectURL(stream) // for older browsers
+//         }
+//
+//         remoteVideo.play()
+//     })
+// }
